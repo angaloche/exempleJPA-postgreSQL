f76179d08cea0da6eb33c22dec6f63e2f218ea61
Harchaoui, Ahmed <ahmed.harchaoui@sap.com>
2019-03-11 22:06:37 -0400

    update

--- script.sql
+++ script.sql
@@ -1,3 +1,12 @@
+create database postgres
+  with owner i840077;
+
+comment on database postgres is 'default administrative connection database';
+
+create sequence customer_id_seq;
+
+alter sequence customer_id_seq owner to i840077;
+
 create table customer
 (
   name varchar,
@@ -7,6 +16,7 @@ create table customer
       primary key
 );
 
-
+alter table customer
+  owner to i840077;
 
 
